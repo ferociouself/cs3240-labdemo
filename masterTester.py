6a32d1e06b11da5725f4d8f1449eff608c4dd0d5
ferociouself <jhe3cd@virginia.edu>
2016-09-26 14:31:38 -0400

    Added a master tester

---
+++ masterTester.py
@@ -0,0 +1,7 @@
+import helper
+
+
+def masterTesting():
+    print("This file is in the master, and it tests helper too!")
+    helper.greeting("Testing a message! We are all good!")
+    print("This test is now ending. If nothing else was printed, we are not good.")
