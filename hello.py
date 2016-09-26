9920871beb2c97c3177bcf2f58526a725401e452
ferociouself <jhe3cd@virginia.edu>
2016-09-26 14:15:34 -0400

    Added a function to hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
+def greeting(msg):
+    print(msg)
+
 if __name__ == "__main__":
-  print("Hello!")
-  x = 4
-  y = 10
-  print("x is " + x.__str__() + ", y is " + y.__str__() ". x + y is " + (x + y).__str__() + ".")
+    print("Hello!")
+    x = 4
+    y = 10
+    print("x is " + x.__str__() + ", y is " + y.__str__() + ". x + y is " + (x + y).__str__() + ".")
+
