30ffcb0c419f12bd8482e8f778faac2fd534dbd7
ferociouself <jhe3cd@virginia.edu>
2016-09-26 14:16:59 -0400

    Testing hello.py

--- hello.py
+++ hello.py
@@ -2,8 +2,5 @@ def greeting(msg):
     print(msg)
 
 if __name__ == "__main__":
-    print("Hello!")
-    x = 4
-    y = 10
-    print("x is " + x.__str__() + ", y is " + y.__str__() + ". x + y is " + (x + y).__str__() + ".")
+    greeting("Hello!")
 
